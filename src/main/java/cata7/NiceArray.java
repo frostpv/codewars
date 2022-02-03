65acd1dd3a86782aef32177371cb44a4f5a22285
frost <frostpv@gmail.com>
2022-02-03 21:01:11 +0200

    NiceArray completed

--- src/main/java/cata7/NiceArray.java
+++ src/main/java/cata7/NiceArray.java
@@ -1,8 +1,6 @@
 package cata7;
 
 import java.util.Arrays;
-import java.util.List;
-
 
 public class NiceArray {
     /**
@@ -10,13 +8,11 @@ public class NiceArray {
      * there is also an element n-1 or n+1 in the array.
      **/
     public static boolean isNice(Integer[] arr) {
-        List<Integer> integers = Arrays.asList(arr);
         boolean res = false;
         for (Integer integer : arr) {
-            if (integers.contains(integer + 1) || integers.contains(integer - 1)) {
-                res = true;
-            } else {
-                res = false;
+            res = Arrays.asList(arr).contains(integer + 1) || Arrays.asList(arr).contains(integer - 1);
+            if (!res) {
+                break;
             }
         }
         return res;
