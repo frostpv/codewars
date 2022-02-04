f4c3ee81eb8494b71676f7086a74c6711a8a4404
frost <frostpv@gmail.com>
2022-02-04 23:11:44 +0200

    refactored kata

--- src/main/java/cata7/BingoOrNot.java
+++ src/main/java/cata7/BingoOrNot.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class BingoOrNot {
-    private static char[] bingo = new char[]{'B', 'I', 'N', 'G', 'O'};
+    private static final char[] bingo = new char[]{'B', 'I', 'N', 'G', 'O'};
+    private static final String LOSE = "LOSE";
+    private static final String WIN = "WIN";
 
     public static String bingo(int[] numberArray) {
         for (char c : bingo) {
             if (!Arrays.stream(numberArray).boxed().collect(Collectors.toList()).contains((int) c - 64)) {
-                return "LOSE";
+                return LOSE;
             }
         }
-        return "WIN";
+        return WIN;
     }
 }
