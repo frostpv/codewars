fb485b987f994082004d971fe9304c07d4ccc5af
vasilijpetecuk <frostpv@gmail.com>
2022-06-20 12:49:56 -0700

    [REFACTOR][KATA] - test git push and refactor

--- src/main/java/cata7/AlphabetSymmetry.java
+++ src/main/java/cata7/AlphabetSymmetry.java
@@ -4,11 +4,17 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class AlphabetSymmetry {
+    private static final int CHAR_VALUE = 65;
+
     public static int[] solve(String[] arr){
        return Stream.of(arr)
                .mapToInt(str -> (int) IntStream.range(0 ,str.length())
-                       .filter(i -> i == str.toUpperCase().charAt(i)-65)
+                       .filter(intValue -> intValue == getAnInt(str, intValue))
                        .count())
                .toArray();
+    }
+
+    private static int getAnInt(String str, int intValue) {
+        return str.toUpperCase().charAt(intValue) - CHAR_VALUE;
     }
 }
